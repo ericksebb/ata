45af9424a76e98fa65801827caa13599aa03d545
E. Sebastián Prado <esprado@sudamericano.edu.ec>
2024-10-20 16:38:16 -0500

    Add code (true-table)

--- m1/ta1/true-table/true-table.ino
+++ m1/ta1/true-table/true-table.ino
@@ -1,9 +1,34 @@
-void setup() {
-  // put your setup code here, to run once:
+int buttonA = 36; 
+int buttonB = 35; 
+int buttonC = 25; 
+int ledPin = 2;  
 
+void setup() {
+
+  pinMode(buttonA, INPUT);
+  pinMode(buttonB, INPUT);
+  pinMode(buttonC, INPUT);
+  
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int A = digitalRead(buttonA);
+  int B = digitalRead(buttonB);
+  int C = digitalRead(buttonC);
 
+  pinMode(buttonB, INPUT_PULLUP);
+  //  
+  int F = (A && B && C) || (!A && B) || (!B && C);
+  
+
+  if (F) {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("F = 1");
+  } else {
+    digitalWrite(ledPin, LOW);  
+    Serial.println("F = 0");
+  }
+  delay(250);
 }
